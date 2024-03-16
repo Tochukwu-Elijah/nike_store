70042a70252f337a506306b61f440131377a463e
Eze Tochukwu <157438236+Tochukwu-Elijah@users.noreply.github.com>
2024-03-16 23:29:01 +0100

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -111,8 +111,8 @@ export const footerLinks = [
     {
         title: "Get in touch",
         links: [
-            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-            { name: "+92554862354", link: "tel:+92554862354" },
+            { name: "elijahtinoet@gmail.com", link: "mailto:elijahtinoet@gmail.com" },
+            { name: "+9065625149", link: "tel:+9065625149" },
         ],
     },
 ];
